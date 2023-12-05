5760d25b665435ce574b511ae7336747b451bbb5
DavidJanssonDev <138443239+DavidJanssonDev@users.noreply.github.com>
2023-12-05 08:32:17 +0100

    comit

--- game_class.js
+++ game_class.js
@@ -1,7 +1,60 @@
-class Player {
-  constructor(hp, dmg, speed) {
+export default class Player {
+  constructor(hp, dmg, acceleration) {
     this.hp = hp;
     this.dmg = dmg;
-    this.speed = s;
+    this.acceleration = acceleration;
+    this.velocityX = 0;
+    this.velocityY = 0;
+    this.x = 0;
+    this.y = 0;
+    this.prevX = 0;
+    this.prevY = 0;
+    this.interpolationFactor = 0.1;
+    this.maxVelocity = 2;
+  }
+
+  update() {
+    this.x = this.prevX + this.interpolationFactor * (this.x - this.prevX);
+    this.y = this.prevY + this.interpolationFactor * (this.y - this.prevY);
+
+    this.velocityX += this.accelerationX;
+    this.velocityY += this.accelerationY;
+
+    const magnitude = Math.sqrt(this.velocityX ** 2 + this.velocityY ** 2);
+    if (magnitude > this.maxVelocity) {
+      const scale = this.maxVelocity / magnitude;
+      this.velocityX *= scale;
+      this.velocityY *= scale;
+    }
+
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+
+    this.prevX = this.x;
+    this.prevY = this.y;
+  }
+
+  accelerate(accelerationX, accelerationY) {
+    const magnitude = Math.sqrt(accelerationX ** 2 + accelerationY ** 2);
+    if (magnitude > 0) {
+      const scale = this.acceleration / magnitude;
+      this.accelerationX = accelerationX * scale;
+      this.accelerationY = accelerationY * scale;
+    } else {
+      this.accelerationX = 0;
+      this.accelerationY = 0;
+    }
+  }
+
+  decelerate(decelerationX, decelerationY) {
+    this.accelerationX = -decelerationX * this.acceleration;
+    this.accelerationY = -decelerationY * this.acceleration;
+  }
+
+  renderer(ctx) {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    // Add rendering logic here to draw the player on the canvas
+    // You might use the canvas and ctx variables from your main.js file
   }
 }
--- main.js
+++ main.js
@@ -9,27 +9,80 @@
     
 */
 
-GAME_OBJECTS = new Array();
+import Player from "./game_class.js";
 
-function game() {
-  GAME_OBJECTS.forEach((index, object) => {
-    console.log(index);
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+const accelerationValue = 0.1;
+
+const player = new Player(100, 10, accelerationValue);
+
+const GAME_OBJECTS = [player];
+
+function gameLoop() {
+  requestAnimationFrame(gameLoop);
+
+  // Logic
+  GAME_OBJECTS.forEach((object) => {
     object.update();
   });
 
+  // Graphics
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Render all game objects, including the player
   GAME_OBJECTS.forEach((object) => {
-    object.renderer();
+    object.renderer(ctx);
   });
 }
 
-class HEJ {
-  update() {
-    console.log("UPDATED");
+// Event listener for keydown
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+    case "W":
+    case "w":
+      player.accelerate(0, -1);
+      break;
+    case "ArrowDown":
+    case "S":
+    case "s":
+      player.accelerate(0, 1);
+      break;
+    case "ArrowLeft":
+    case "A":
+    case "a":
+      player.accelerate(-1, 0);
+      break;
+    case "ArrowRight":
+    case "D":
+    case "d":
+      player.accelerate(1, 0);
+      break;
   }
-  renderer() {
-    console.log("Rendered");
-  }
-}
-GAME_OBJECTS.push(new HEJ());
+});
 
-game();
+// Event listener for keyup
+document.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+    case "W":
+    case "w":
+    case "ArrowDown":
+    case "S":
+    case "s":
+      player.decelerate(0, player.velocityY);
+      break;
+    case "ArrowLeft":
+    case "A":
+    case "a":
+    case "ArrowRight":
+    case "D":
+    case "d":
+      player.decelerate(player.velocityX, 0);
+      break;
+  }
+});
+
+gameLoop();
